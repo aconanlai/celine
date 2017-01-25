f0342fe798d509e2b3416aa0a02570ce164c3cbb
Conan <aconanlai@gmail.com>
2017-01-24 23:57:38 -0500

    fix language errors

--- src/components/ContentPanel/Contact/Contact.js
+++ src/components/ContentPanel/Contact/Contact.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 class Contact extends React.Component {
   render() {
-    const text = (this.props.selectedLang === 'eng') ? this.props.contact : this.props.contactFr;
+    const text = (this.props.selectedLang === 'en') ? this.props.contact : this.props.contactFr;
     return (
-      <div className="contact" dangerouslySetInnerHTML={{ __html: text }} />
+      <div dangerouslySetInnerHTML={{ __html: text }} />
     );
   }
 }
--- src/components/ContentPanel/Writing/Writing.js
+++ src/components/ContentPanel/Writing/Writing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Writing extends React.Component {
   render() {
-    const text = (this.props.selectedLang === 'eng') ? this.props.writing : this.props.writingFr;
+    const text = (this.props.selectedLang === 'en') ? this.props.writing : this.props.writingFr;
     return (
       <div className="writing" dangerouslySetInnerHTML={{ __html: text }} />
     );
